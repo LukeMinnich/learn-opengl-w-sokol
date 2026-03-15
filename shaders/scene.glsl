ee7b3f3c5e3716a42f7fc68d240e894c82c13d90
LukeMinnich <squareman2357@gmail.com>
2026-03-14 21:19:50 -0700

    One happy container

--- shaders/scene.glsl
+++ shaders/scene.glsl
@@ -3,25 +3,37 @@
 @vs scene_vs
 in vec3 aPos;
 in vec3 aColor;
+in vec2 aTexCoord;
 
 out vec3 ourColor;
+out vec2 TexCoord;
 
 void main(
 	void
 ) {
 	gl_Position = vec4(aPos, 1.);
 	ourColor = aColor;
+	TexCoord = aTexCoord;
 }
 @end
 
 @fs scene_fs
-out vec4 FragColor;
+layout(binding=0) uniform texture2D boxTexture;
+layout(binding=0) uniform sampler boxSampler;
+layout(binding=1) uniform texture2D faceTexture;
+layout(binding=1) uniform sampler faceSampler;
+
 in vec3 ourColor;
+in vec2 TexCoord;
+
+out vec4 FragColor;
 
 void main(
 	void
 ) {
-	FragColor = vec4(ourColor, 1.);
+	FragColor = mix(texture(sampler2D(boxTexture, boxSampler), TexCoord),
+                  texture(sampler2D(faceTexture, faceSampler), TexCoord),
+                  0.2);
 }
 @end
 
--- src/main.c
+++ src/main.c
@@ -6,22 +6,31 @@
 #include "sokol_log.h"
 #include "sokol_time.h"
 
-#include <math.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #include <stdlib.h>
 
 typedef float f32;
 typedef uint16_t u16;
+typedef unsigned char byte;
 
 static const f32 vertices[] = {
-	 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-	-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-	 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,   // top left
+};
+static const u16 indices[] = {
+	0, 1, 3,
+	1, 2, 3,
 };
 
 typedef struct {
 	sg_pipeline pipeline;
 	sg_bindings bindings;
 	sg_pass_action pass_action;
+	byte *image_data;
 } AppState;
 
 #define countof(x) (sizeof(x) / sizeof((x)[0]))
@@ -30,6 +39,33 @@ static void
 state_init(
 	AppState *state
 ) {
+	stbi_set_flip_vertically_on_load(true);
+	int w, h, n_channels;
+	byte *box_data = stbi_load("assets/container.jpg", &w, &h, &n_channels, 4);
+	assert(box_data);
+	sg_image box_img = sg_make_image(&(sg_image_desc){
+		.width = w,
+		.height = h,
+		.pixel_format = SG_PIXELFORMAT_RGBA8,
+		.data.mip_levels[0] = {
+			.ptr = box_data,
+			.size = (size_t)(w * h * 4),
+		},
+	});
+	stbi_image_free(box_data);
+	byte *face_data = stbi_load("assets/awesomeface.png", &w, &h, &n_channels, 4);
+	assert(face_data);
+	sg_image face_img = sg_make_image(&(sg_image_desc){
+		.width = w,
+		.height = h,
+		.pixel_format = SG_PIXELFORMAT_RGBA8,
+		.data.mip_levels[0] = {
+			.ptr = face_data,
+			.size = (size_t)(w * h * 4),
+		},
+	});
+	stbi_image_free(face_data);
+
 	*state = (AppState){
 		.pipeline = sg_make_pipeline(&(sg_pipeline_desc){
 			.shader = sg_make_shader(scene_shader_desc(sg_query_backend())),
@@ -37,14 +73,39 @@ state_init(
 				.attrs = {
 					[ATTR_scene_aPos].format = SG_VERTEXFORMAT_FLOAT3,
 					[ATTR_scene_aColor].format = SG_VERTEXFORMAT_FLOAT3,
+					[ATTR_scene_aTexCoord].format = SG_VERTEXFORMAT_FLOAT2,
 				}
 			},
+			.index_type = SG_INDEXTYPE_UINT16,
 			.label = "scene-pipeline"
 		}),
 		.bindings.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
 			.usage.vertex_buffer = true,
 			.data = SG_RANGE(vertices),
 			.label = "vertex-buffer",
+		}),
+		.bindings.index_buffer = sg_make_buffer(&(sg_buffer_desc){
+			.usage.index_buffer = true,
+			.data = SG_RANGE(indices),
+			.label = "index-buffer",
+		}),
+		.bindings.views[VIEW_boxTexture] = sg_make_view(&(sg_view_desc){
+			.texture = {.image = box_img },
+			.label = "box-texture",
+		}),
+		.bindings.samplers[SMP_boxSampler] = sg_make_sampler(&(sg_sampler_desc){
+			.min_filter = SG_FILTER_LINEAR,
+			.mag_filter = SG_FILTER_LINEAR,
+			.label = "box-sampler"
+		}),
+		.bindings.views[VIEW_faceTexture] = sg_make_view(&(sg_view_desc){
+			.texture = {.image = face_img },
+			.label = "face-texture",
+		}),
+		.bindings.samplers[SMP_faceSampler] = sg_make_sampler(&(sg_sampler_desc){
+			.min_filter = SG_FILTER_LINEAR,
+			.mag_filter = SG_FILTER_LINEAR,
+			.label = "face-sampler"
 		}),
 		.pass_action = (sg_pass_action) {
 			.colors[0] = {
@@ -66,7 +127,7 @@ app_frame(
 	});
 	sg_apply_pipeline(state->pipeline);
 	sg_apply_bindings(&state->bindings);
-	sg_draw(0, countof(vertices) / 6, 1);
+	sg_draw(0, countof(indices), 1);
 	sg_end_pass();
 	sg_commit();
 }
